e84dadef3442f77483a48e52974a9affaa976b12
GitanAirfrier <jsanchezdominguez57@gmail.com>
2024-09-20 10:47:17 +0200

    01 y 02

--- 02_Estructura_de_control/index.php
+++ 02_Estructura_de_control/index.php
@@ -6,6 +6,18 @@
     <title>Index</title>
 </head>
 <body>
-    
+    <?php
+    $numero = "a";
+
+    if($numero > 0) echo "<p>El numero $numero es positivo</p>";
+    if($numero < 0):
+        echo "<p>El número $numero es negativo</p>";
+    endif;
+    if($numero > 0) {
+        echo "El numero $numero es positivo";
+    }else {
+        echo "<p>El número $numero es negativo</p>";
+    }
+    ?>
 </body>
 </html>
